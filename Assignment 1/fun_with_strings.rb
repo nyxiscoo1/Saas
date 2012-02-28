c455affec1f300a69ad146cf9e933fbebfad03a9
nyx <nyx@mail.ru>
2012-02-29 00:40:24 +0400

    Fixed fun with strings

--- Assignment 1/fun_with_strings.rb
+++ Assignment 1/fun_with_strings.rb
@@ -1,12 +1,12 @@
-def palindrom?(str)
+def palindrome?(str)
     downcased = str.gsub(/\W/, '').downcase()
     downcased == downcased.reverse()
 end
 
-def word_count(str)
+def count_words(str)
     Hash[str.gsub(/\W/, ' ').downcase.split.group_by{|x| x}.collect{|x| [x[0], x[1].length]}]
 end
 
 if __FILE__ == $0
-    puts word_count ARGV[0].chomp
+    puts count_words ARGV[0].chomp
 end
--- Assignment 1/fun_with_strings_tests.rb
+++ Assignment 1/fun_with_strings_tests.rb
@@ -1,26 +1,26 @@
 require 'fun_with_strings.rb'
 
-describe "palindrom?" do
+describe "palindrome?" do
     it "should match first" do
-        (palindrom? 'A man, a plan, a canal -- Panama').should eq(true)
+        (palindrome? 'A man, a plan, a canal -- Panama').should eq(true)
     end
     
     it "should match second" do
-        (palindrom? 'Madam, I`m Adam!').should be(true)
+        (palindrome? 'Madam, I`m Adam!').should be(true)
     end
     
     it "should match third" do
-        (palindrom? 'Abracadabra').should be(false)
+        (palindrome? 'Abracadabra').should be(false)
     end
 end
 
-describe "word_count" do
+describe "count_words" do
     it "should match first" do
-        (word_count 'A man, a plan, a canal -- Panama').should eq({'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1})
+        (count_words 'A man, a plan, a canal -- Panama').should eq({'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1})
     end
     
     it "should match second" do
-        (word_count 'Doo bee doo bee doo').should eq({'doo' => 3, 'bee' => 2})
+        (count_words 'Doo bee doo bee doo').should eq({'doo' => 3, 'bee' => 2})
     end
     
     it "should match third" do
